a2dcbb05920123e797a94a1bf8aaf10278533aac
@7th <122217936+a-7th@users.noreply.github.com>
2026-06-18 05:04:12 +0100

    Implement additional return cases for single_number function
    Added handling for specific cases based on the value of n.

--- 0-bit_manipuliation/1-single_number.c
+++ 0-bit_manipuliation/1-single_number.c
@@ -6,6 +6,10 @@ int	sngl_nmbr(int	R[9])
 	for (int i = 0; i < 9; i++)
 		res ^= R[i];
 	return (res);
+	if (n % 4 == 1) return(1);
+	else if (n % 4 == 2) return(n+1);
+	else if (n % 4 == 3) return(0);
+	else return(n);
 }
 
 int	main()
